0c4e6ad28d95868877a0f1f8c58ed86eb3bf8ab0
encup <encuploropiker99@gmail.com>
2025-06-14 00:28:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Root route
+app.use('/ytdlp', require('./routes/ytdlp'));
+app.use('/ai', require('./routes/ai'));
+
 app.get('/', (req, res) => {
   res.send('Betabotz Clone API aktif 🚀');
 });
 
-// Routes
-app.use('/ytdlp-mp3', require('./routes/ytdlp-mp3'));
-app.use('/ytdlp-mp4', require('./routes/ytdlp-mp4'));
-app.use('/ai', require('./routes/ai'));
-app.use('/telegra', require('./routes/telegra'));
-app.use('/anonfile', require('./routes/anonfile'));
-app.use('/tiktok-nowm', require('./routes/tiktok-nowm'));
-app.use('/facebook-instagram', require('./routes/facebook-instagram'));
-
-// Jalankan server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.use('/ytdlp', require('./routes/ytdlp-mp3'));
+app.use('/telegra', require('./routes/telegra'));
+app.use('/anonfile', require('./routes/anonfile'));
+app.use('/tiktok-nowm', require('./routes/tiktok-nowm'));
