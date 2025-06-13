537f563991eeae14c99b51792384a32392bfc7a0
encup <encuploropiker99@gmail.com>
2025-06-14 00:55:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const port = process.env.PORT || 3000;
 
 app.use(fileUpload());
 
-app.use('/ytdlp', require('./routes/ytdlp'));
-app.use('/ytdlp-mp3', require('./routes/ytdlp-mp3'));
-app.use('/tiktok-nowm', require('./routes/tiktok-nowm'));
-app.use('/telegra', require('./routes/telegra'));
-app.use('/anonfile', require('./routes/anonfile'));
-app.use('/ai', require('./routes/ai'));
+app.use('/api/ytdlp', require('./routes/ytdlp'));
+app.use('/api/ytdlp-mp3', require('./routes/ytdlp-mp3'));
+app.use('/api/tiktok-nowm', require('./routes/tiktok-nowm'));
+app.use('/api/telegra', require('./routes/telegra'));
+app.use('/api/anonfile', require('./routes/anonfile'));
+app.use('/api/ai', require('./routes/ai'));
 
 app.get('/', (req, res) => {
   res.send('Betabotz Clone API aktif 🚀');
