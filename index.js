7cb4c6bae22a372fa712a75365ee15320b6fb51a
encup <encuploropiker99@gmail.com>
2025-06-14 00:36:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
 const port = process.env.PORT || 3000;
 
+app.use(fileUpload());
+
 app.use('/ytdlp', require('./routes/ytdlp'));
+app.use('/ytdlp-mp3', require('./routes/ytdlp-mp3'));
+app.use('/tiktok-nowm', require('./routes/tiktok-nowm'));
+app.use('/telegra', require('./routes/telegra'));
+app.use('/anonfile', require('./routes/anonfile'));
 app.use('/ai', require('./routes/ai'));
 
 app.get('/', (req, res) => {
@@ -12,8 +19,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-app.use('/ytdlp', require('./routes/ytdlp-mp3'));
-app.use('/telegra', require('./routes/telegra'));
-app.use('/anonfile', require('./routes/anonfile'));
-app.use('/tiktok-nowm', require('./routes/tiktok-nowm'));
